5336707feea98771d0a2c1c037fd365baeef8ffe
Bhanavi Goyal <bhanavigoyal@gmail.com>
2024-11-25 22:19:16 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express =require("express");
+const app = express();
+const PORT = 4000;
+
+
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors());
+
+app.get('/api',(req, res)=>{
+    res.json({
+        message: "Hello world",
+    });
+});
+
+http.listen(PORT, ()=>{
+    console.log(`Server running on PORT: ${PORT}`)
+});

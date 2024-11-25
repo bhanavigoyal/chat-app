c0b24fc1068f1d1c824ccc25b15e544f3a68a589
Bhanavi Goyal <bhanavigoyal@gmail.com>
2024-11-25 22:23:44 +0530

    implemented basic socket connection

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,21 @@ const cors = require("cors");
 
 app.use(cors());
 
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+    });
+});
+
+
 app.get('/api',(req, res)=>{
     res.json({
         message: "Hello world",
